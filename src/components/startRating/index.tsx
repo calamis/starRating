dc20412ad5adde476dd47b877c81acab49b8a48a
calamis <mekorn_damboy@yahoo.com>
2022-05-27 18:23:30 +0800

    feat: test commit

--- src/components/startRating/index.tsx
+++ src/components/startRating/index.tsx
@@ -20,7 +20,6 @@ function StarRating({
       {createArray(totalStars).map((n, i) => (
         <Star key={i} selected={selectedStars > i} onSelect={() => onRate(i + 1)} />
       ))}
-
       <p>
         {selectedStars} of {totalStars} stars
       </p>
